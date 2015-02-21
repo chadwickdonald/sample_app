f4d8b3277d40d7ca035c10a2a78d82e3f95182ba
chadwickdonald <chadwickbidwell@gmail.com>
2015-02-21 08:45:49 -0800

    fixing tests for edit and update

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -1,15 +1,13 @@
 require 'test_helper'
 
 class UsersEditTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
 
   def setup
   	@user = users(:michael)
   end
 
   test "unsuccessful edit" do
+  	log_in_as(@user)
   	get edit_user_path(@user)
   	assert_template "users/edit"
   	patch user_path(@user), user: { name: " ",
@@ -20,6 +18,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "successful edit" do
+  	log_in_as(@user)
   	get edit_user_path(@user)
   	assert_template "users/edit"
   	name = "donkey guy"
